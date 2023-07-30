2af9cfdda87f75db6854a342e0d94006e5c1d00f
R O M A <109772373+newkamaji@users.noreply.github.com>
2023-07-30 06:41:38 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,21 +7,24 @@ MAX_GWEI    = 16 # gas в gwei (смотреть здесь : https://etherscan.
 
 RANDOM_WALLETS  = True # True / False
 
-RUN_FOREVER = True # True / False
+MAX_CYCLE = 5
 
-from_sec = 1       #|Wait from N seconds between transactions | Минимальное значение "ждать от N sec между транзакиями". Для рандомного выбора
-to_sec = 100	   #|Wait to N seconds between transactions | Максиимальное значение "спать до N sec между транзакциями". Для рандомного выбора
+from_sec = 30       #|Wait from N seconds between transactions | Минимальное значение "ждать от N sec между транзакиями". Для рандомного выбора
+to_sec = 50	   #|Wait to N seconds between transactions | Максиимальное значение "спать до N sec между транзакциями". Для рандомного выбора
 eth_min = 0.0001   #|Min ETH quantity for deposit | Значение ETH минимального депозита. Для рандомного выбора
 eth_max = 0.0001   #|Max ETH quantity for deposit | Значение ETH максимального депозита. Для рандомного выбора
 
-contract_address = "0xBd31E4c755580e97C54d6Ac55AD7B6Ae0D6bEFbb" #|Contract address. DO NOT CHANGE if own contract is not deployed | Адрес контракта. НЕ МЕНЯТЬ если не свой не деплоили
-
+#|Contract address. DO NOT CHANGE if own contract is not deployed | Адрес контракта. НЕ МЕНЯТЬ если не свой не деплоили
+#contract_address = "" # 1
 
 RPC = "https://mainnet.era.zksync.io" #|RPC for web3 provider. DO NOT CHANGE if you dont have own RPC | RPC web3 провайдера. НЕ МЕНЯТЬ если нет своей
 #----------#
 
 swap_all_balance = True #flag True / False
-keep_value_from = 0.0008   # от скольки монет оставлять
+keep_value_from = 0.0006   # от скольки монет оставлять
 keep_value_to   = 0.0009  # до скольки монет оставлять
+
+keep_withdraw_value_from = 0.00006   # от скольки монет оставлять
+keep_withdraw_value_to   = 0.00009  # до скольки монет оставлять
 #----------#
 
